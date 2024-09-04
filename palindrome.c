aa868aca7f48631420604b9c58192aa2cfebc343
countz-zero <32971308+countz-zero@users.noreply.github.com>
2024-09-04 13:41:26 +0700

    File for example

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void) {
+
+    char *word = "racecars";
+    int length = strlen(word);
+    bool is_palindrome = true; 
+
+    for (int i = 0; i < length/2; i++) {
+        if (word[i] != word[length - i - 1]) {
+            is_palindrome = false;
+            break;
+        }
+    }
+
+    if (is_palindrome) printf("Palindrome!");
+    else printf("Not a palindrome");
+
+    return 0;
+}
